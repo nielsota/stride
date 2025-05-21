ee6a854f5a89b17f1fcebc3d729c32358a322303
Niels Ota <ota.niels@bcg.com>
2025-05-21 11:36:47 +0200

    Update dependencies to include pydantic-settings and refactor StravaConfig to inherit from pydantic_settings.BaseSettings for improved env vars management.

--- src/stride/config.py
+++ src/stride/config.py
@@ -1,8 +1,7 @@
 import pydantic
+import pydantic_settings
 from datetime import datetime
-import os
 from pydantic import Field
-from dotenv import load_dotenv
 
 
 class BaseConfig(pydantic.BaseModel):
@@ -11,7 +10,7 @@ class BaseConfig(pydantic.BaseModel):
     env_path: str = ".env"
 
 
-class StravaConfig(BaseConfig):
+class StravaConfig(pydantic_settings.BaseSettings):
     """Configuration for the Strava connection."""
 
     client_id: str = Field(alias="STRAVA_CLIENT_ID")
@@ -20,25 +19,15 @@ class StravaConfig(BaseConfig):
     access_token: str = Field(alias="STRAVA_ACCESS_TOKEN")
     refresh_token: str = Field(alias="STRAVA_REFRESH_TOKEN")
     expires_at: datetime = Field(alias="STRAVA_ACCESS_TOKEN_EXPIRES_AT")
-    
-    # TODO: use model config to set env_file to .env
 
-    @classmethod
-    def from_env(cls) -> "StravaConfig":
-        """Load the configuration from the environment variables."""
-        # Force reload by finding the .env file and loading it with override
-        load_dotenv(override=True)
-
-        fields_aliases = [field.alias for field in cls.model_fields.values()]
-        env_vars = {k: v for k, v in os.environ.items() if k in fields_aliases}
-        return cls(**env_vars)
+    model_config = pydantic_settings.SettingsConfigDict(env_file=".env")
 
     def get_bearer_token(self) -> str:
         """Get the bearer token for the Strava API."""
         return f"Bearer {self.access_token}"
 
 
-strava_config = StravaConfig.from_env()
+strava_config = StravaConfig()
 
 if __name__ == "__main__":
     print(strava_config)
